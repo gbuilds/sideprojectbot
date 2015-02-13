7cd5d2ba6cccfa32a40def01523657816e1bd6b4
Gordie Smith <agsmith4@gmail.com>
2015-02-13 22:29:27 +0000

    Set config/env/production.rb serve static assets true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or NGINX will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  config.assets.digest = false
 
 # TODO configure actual URL of website on Heroku
 # config.action_mailer.default_url_options = { host:  } 

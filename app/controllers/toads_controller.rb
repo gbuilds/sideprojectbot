443404f75cff5a22c4cb0962b93fa18fc71274cd
Gordie Smith <agsmith4@gmail.com>
2015-02-14 03:22:50 +0000

    Allow for user to delete project toads

--- app/controllers/toads_controller.rb
+++ app/controllers/toads_controller.rb
@@ -1,4 +1,6 @@
 class ToadsController < ApplicationController
+  before_action :logged_in_user?
+  before_action :correct_user?
   
   def create
     @project = Project.find(params[:project_id])
@@ -6,9 +8,28 @@ class ToadsController < ApplicationController
     redirect_to project_path(@project)
   end
   
+  def destroy    
+    @project = Project.find(params[:project_id])
+    @toad = Toad.find(params[:id])
+    @toad.delete
+    redirect_to @project
+  end
+  
   private
   
   def toad_params
     params.require(:toad).permit(:description, :complete)
+  end
+  
+  def logged_in_user?
+    unless user_signed_in?
+      flash[:warning] = "You are not signed in."
+      redirect_to new_user_session_path
+    end
+  end
+  
+  def correct_user?
+    @project = current_user.projects.find_by(id: params[:project_id])
+    redirect_to root_url if @project.nil? 
   end
 end
